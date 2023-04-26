14bed11c6783a03b4c02de4689281d533100aa3d
stevecyj <ycsteven.tsao@gmail.com>
2023-04-26 14:45:01 +0800

    style: add Center component to Chakra UI navbar
    - Add `Center` component from Chakra UI to center the image and text in the navbar.
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Container, Box, Flex, Text, Image } from '@chakra-ui/react';
+import { Container, Box, Flex, Text, Image, Center } from '@chakra-ui/react';
 import CoffeeLogo from './coffee.svg';
 
 export default function Home() {
@@ -7,10 +7,12 @@ export default function Home() {
       <Container maxW={'1200px'} w={'100%'}>
         {/*Navbar*/}
         <Flex px='10px' bg={{ base: '#fff' }} h={'120px'} borderRadius={'20px'} boxShadow={'lg'}>
-          <Image src={CoffeeLogo} w={50} h={50} alt='Buy Me a Coffee' />
-          <Text w={'100%'} fontWeight={600} fontSize={'24px'}>
-            Buy Me A Coffee
-          </Text>
+          <Center>
+            <Image src={CoffeeLogo} w={50} h={50} alt='Buy Me a Coffee' />
+            <Text w={'100%'} fontWeight={600} fontSize={'24px'}>
+              Buy Me A Coffee
+            </Text>
+          </Center>
         </Flex>
       </Container>
     </Box>
