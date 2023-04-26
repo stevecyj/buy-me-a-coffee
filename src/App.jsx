342c39128a268c4c0d2ddd097a65d5ff07bfcbe3
stevecyj <ycsteven.tsao@gmail.com>
2023-04-26 14:43:08 +0800

    style: add branding elements to navbar using Chakra UI components
    - Add a Buy Me A Coffee logo and text to the navbar
    - Import the `Text` and `Image` components from `@chakra-ui/react`
    - Change the background color to use single quotes instead of double quotes
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { Container, Box, Flex } from '@chakra-ui/react';
+import { Container, Box, Flex, Text, Image } from '@chakra-ui/react';
+import CoffeeLogo from './coffee.svg';
 
 export default function Home() {
   return (
-    <Box bg="#fefefe" w={'100%'} h={'100%'}>
+    <Box bg='#fefefe' w={'100%'} h={'100%'}>
       <Container maxW={'1200px'} w={'100%'}>
         {/*Navbar*/}
-        <Flex px="10px" bg={{ base: '#fff'}} h={'120px'} borderRadius={'20px'} boxShadow={'lg'}></Flex>
+        <Flex px='10px' bg={{ base: '#fff' }} h={'120px'} borderRadius={'20px'} boxShadow={'lg'}>
+          <Image src={CoffeeLogo} w={50} h={50} alt='Buy Me a Coffee' />
+          <Text w={'100%'} fontWeight={600} fontSize={'24px'}>
+            Buy Me A Coffee
+          </Text>
+        </Flex>
       </Container>
     </Box>
   );
