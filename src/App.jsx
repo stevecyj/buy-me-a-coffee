c5c2f8b8321e4cb5c6a17b5d1ffc059349a0a123
stevecyj <ycsteven.tsao@gmail.com>
2023-04-26 15:20:04 +0800

    feat: refactor navbar to include ConnectWallet button and component import
    - Change the print width from `100` to `80` in the `.prettierrc.cjs` file
    - Add the `ConnectWallet` component import in `src/App.jsx`
    - Add a `ConnectWallet` button to the navbar in `src/App.jsx`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Container, Box, Flex, Text, Image, Center } from '@chakra-ui/react';
 import CoffeeLogo from './coffee.svg';
+import { ConnectWallet } from '@thirdweb-dev/react';
 
 export default function Home() {
   return (
@@ -7,11 +8,14 @@ export default function Home() {
       <Container maxW={'1200px'} w={'100%'}>
         {/*Navbar*/}
         <Flex px='10px' bg={{ base: '#fff' }} h={'120px'} borderRadius={'20px'} boxShadow={'lg'}>
-          <Center w='100'>
+          <Center w='100%'>
             <Image src={CoffeeLogo} w={50} h={50} alt='Buy Me a Coffee' />
             <Text w={'100%'} fontWeight={600} fontSize={'24px'}>
               Buy Me A Coffee
             </Text>
+            <Box mr='2rem'>
+              <ConnectWallet btnTitle='連接錢包' />
+            </Box>
           </Center>
         </Flex>
       </Container>
