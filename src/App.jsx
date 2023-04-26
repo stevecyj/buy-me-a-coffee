22df5f02f2d8ea8eb22e5f8900cff37c44d71ad5
stevecyj <ycsteven.tsao@gmail.com>
2023-04-26 14:48:00 +0800

    style: fix navbar logo centering in UI
    - Fix centering of logo in navbar
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Container maxW={'1200px'} w={'100%'}>
         {/*Navbar*/}
         <Flex px='10px' bg={{ base: '#fff' }} h={'120px'} borderRadius={'20px'} boxShadow={'lg'}>
-          <Center>
+          <Center w='100'>
             <Image src={CoffeeLogo} w={50} h={50} alt='Buy Me a Coffee' />
             <Text w={'100%'} fontWeight={600} fontSize={'24px'}>
               Buy Me A Coffee
