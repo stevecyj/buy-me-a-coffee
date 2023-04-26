96bc98e4b88f23edd1eea8d3d8160964239b5cff
stevecyj <ycsteven.tsao@gmail.com>
2023-04-26 14:30:02 +0800

    feat: add navigation to the app component
    - Add a navbar to the App component
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Container, Box } from '@chakra-ui/react';
+import { Container, Box, Flex } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <Box bg="#fefefe" w={'100%'} h={'100%'}>
-      Home
+      <Container maxW={'1200px'} w={'100%'}>
+        {/*Navbar*/}
+        <Flex px="10px" bg={{ base: '#fff'}} h={'120px'} borderRadius={'20px'} boxShadow={'lg'}></Flex>
+      </Container>
     </Box>
   );
 }
